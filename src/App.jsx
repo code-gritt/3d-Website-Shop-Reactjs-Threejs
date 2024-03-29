379c1026b790a9b4780e11a96646f5da94aa4f16
Gokul <gokulchandan24@gmail.com>
2024-03-29 13:06:47 +0530

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Home from "./pages/Home";
+import Customizer from "./pages/Customizer";
+import CanvasModel from "./canvas";
+
 function App() {
   return (
     <>
-      <main className="app transition-all ease-in">Gokul</main>
+      <main className="app transition-all ease-in">
+        <Home />
+        {/* <CanvasModel /> */}
+        <Customizer />
+      </main>
     </>
   );
 }
