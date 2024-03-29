18763c37bf16952dbc62381151a1fe2af16d4fdf
Gokul <gokulchandan24@gmail.com>
2024-03-29 13:42:01 +0530

    included everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Home from "./pages/Home";
 import Customizer from "./pages/Customizer";
-import CanvasModel from "./canvas";
+import Canvas from './canvas';
 
 function App() {
   return (
     <>
       <main className="app transition-all ease-in">
         <Home />
-        {/* <CanvasModel /> */}
+        <Canvas />
         <Customizer />
       </main>
     </>
